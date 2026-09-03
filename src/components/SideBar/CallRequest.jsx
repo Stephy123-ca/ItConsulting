b9582b56246640852099f41d8b416d921846e427
Stephy123-ca <stephyantony005@gmail.com>
2026-09-03 12:52:14 +0530

    second

--- src/components/SideBar/CallRequest.jsx
+++ src/components/SideBar/CallRequest.jsx
@@ -6,7 +6,7 @@ const CallRequest = ({onClose,onPhoneClick}) => {
     <>
           <div className=''>
               <div className='relative w-full flex justify-center items-center'>
-                  <div className='font-josefin bg-slate-200/40 backdrop-blur-lg fixed  md:w-[400px] w-[250px] top-1/4 md:left-1/4 lg:left-96 left-12 border text-center border-white/35 shadow-lg p-5  rounded-xl '>
+                  <div className='font-josefin bg-slate-200/40 backdrop-blur-lg fixed  md:w-[400px] w-[250px] h-[460px] top-1/4 md:left-1/4 lg:left-[420px] lg:top-24 left-16 border text-center border-white/35 shadow-lg p-5  rounded-xl '>
 
                       <button
                           onClick={onClose}
@@ -25,9 +25,9 @@ const CallRequest = ({onClose,onPhoneClick}) => {
 
                       <h1 className='text-[24.71px] text-[#ffffff] font-sans font-bold mt-[20px] leading-6 z-99'>Request a Callback</h1>
                       <p className='text-[14px] text-[#ffffff] font-sans mt-2'>Our team will contact you shortly</p>
-                      <input type="text" placeholder="Your Name" className='bg-slate-100/30 backdrop-blur-md focus:placeholder-gray-400 border p-2 rounded mt-5 w-full text-sm text-[#666666] placeholder-gray-300 font-sans ' />
-                      <input type="text" placeholder="Phone Number" className='bg-slate-100/30 backdrop-blur-md border p-2 rounded focus:placeholder-gray-400 mt-5 w-full text-sm text-[#000000] placeholder-gray-300 font-sans' />
-                      <select defaultValue="" className='mt-5 rounded p-2 bg-white text-black text-sm w-full outline-none
+                      <input type="text" placeholder="Your Name" className='bg-slate-100/30 backdrop-blur-md focus:placeholder-gray-400 border p-2 rounded mt-7 w-full text-sm text-[#666666] placeholder-gray-300 font-sans ' />
+                      <input type="text" placeholder="Phone Number" className='bg-slate-100/30 backdrop-blur-md border p-2 rounded focus:placeholder-gray-400 mt-7 w-full text-sm text-[#000000] placeholder-gray-300 font-sans' />
+                      <select defaultValue="" className='mt-7 rounded p-2 bg-white text-black text-sm w-full outline-none
     focus:ring-2
     focus:ring-[#F2C94C]'>
                           <option value="" disabled>Select Service</option>
@@ -37,7 +37,7 @@ const CallRequest = ({onClose,onPhoneClick}) => {
                           <option value="software">Software Development</option>
                           <option value="IT">IT Consulting</option>
                      </select>
-                      <button className='w-full mt-5 font-semibold p-2  rounded bg-[#F2C94C] text-[#0A2f5A] font-sans text-[16px]'>Submit Request</button>
+                      <button className='w-full mt-7 font-semibold p-2  rounded bg-[#F2C94C] text-[#0A2f5A] font-sans text-[16px]'>Submit Request</button>
                   </div>
               </div>
           </div>
--- src/pages/AdminLogin.jsx
+++ src/pages/AdminLogin.jsx
@@ -8,8 +8,7 @@ const AdminLogin = ({ onClose, onAdminClick,onPhoneClick }) => {
     <>
 
               <div className='relative w-full flex justify-center items-center '>
-              <div className='font-josefin fixed inset-0 md:w-[400px] w-[250px]
-            h-[350px] top-1/4 md:left-1/4 lg:left-96 left-12 bg-slate-200/40 text-white backdrop-blur-lg border text-center border-white/20 shadow-lg p-5 rounded-xl
+              <div className='font-josefin fixed inset-0 md:w-[400px] w-[250px] h-[420px] lg:top-24 top-1/4 md:left-1/4 lg:left-[420px] left-16 bg-slate-200/40 text-white backdrop-blur-lg border text-center border-white/20 shadow-lg p-5 rounded-xl
 '>
           
                    <div className='relative'>
@@ -21,7 +20,7 @@ const AdminLogin = ({ onClose, onAdminClick,onPhoneClick }) => {
                               </button>
                    </div>
            
-                      <div className='flex justify-center items-center mt-3'>
+                      <div className='flex justify-center items-center mt-5'>
                           <div className='flex justify-center items-center p-2 w-[50px] h-[50px] bg-[#ffdf00]  rounded-full mt-2'>
                            
                                   <i className="fa-solid fa-user-shield text-[#0A2F5A] text-[32px]"></i>
@@ -31,9 +30,9 @@ const AdminLogin = ({ onClose, onAdminClick,onPhoneClick }) => {
    
                       <h1 className='text-[24.71px] text-[#ffffff] font-sans font-bold mt-[20px] leading-6'>Admin Portal</h1>
                       <p className='text-[14px] text-[#ffffff] font-sans mt-1'>Authorized access only</p>
-                  <input type="text" placeholder="Username" className='bg-slate-100/30 backdrop-blur-md focus:placeholder-gray-400 border p-2 rounded mt-5 w-full text-sm text-[#000000] placeholder-gray-300 font-[500] font-sans ' />
-                  <input type="text" placeholder="Password" className='bg-slate-100/30 backdrop-blur-md focus:placeholder-gray-400 border p-2 rounded mt-5 w-full text-sm text-[#000000] placeholder-gray-300 font-[500] font-sans' />
-                      <button className='w-full mt-5 font-semibold p-2  rounded bg-[#F2C94C] text-[#0A2f5A] font-sans text-[16px]'>Login To Dashboard</button>
+                  <input type="text" placeholder="Username" className='bg-slate-100/30 backdrop-blur-md focus:placeholder-gray-400 border p-2 rounded mt-7 w-full text-sm text-[#000000] placeholder-gray-300 font-[500] font-sans ' />
+                  <input type="text" placeholder="Password" className='bg-slate-100/30 backdrop-blur-md focus:placeholder-gray-400 border p-2 rounded mt-7 w-full text-sm text-[#000000] placeholder-gray-300 font-[500] font-sans' />
+                      <button className='w-full mt-7 font-semibold p-2  rounded bg-[#F2C94C] text-[#0A2f5A] font-sans text-[16px]'>Login To Dashboard</button>
                   </div>
               </div>
    
