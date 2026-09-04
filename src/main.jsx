bbb1723e1172d30937f1b806296a1c8167e8491a
Stephy123-ca <stephyantony005@gmail.com>
2026-09-04 14:55:44 +0530

    finally

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,6 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <BrowserRouter><App /></BrowserRouter>
+    <BrowserRouter basename="/ItConsulting"><App/></BrowserRouter>
   </React.StrictMode>,
 )
